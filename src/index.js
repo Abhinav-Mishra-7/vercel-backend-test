c14f72e6ee4cd79dd421a4e4edce8e86244339f9
Abhinav Mishra <abhinavmishra12514@gmail.com>
2025-07-29 19:47:53 +0530

    Eigteenth Change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 require("dotenv").config() ;
+const main = require("./config/db") ;
+const redisClient = require("./config/redis");
 
 const PORT = process.env.PORT || 5000;
 
