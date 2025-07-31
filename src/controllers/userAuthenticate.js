d50f32be7062de99dc29a2839704022a88280855
Abhinav Mishra <abhinavmishra12514@gmail.com>
2025-07-31 19:19:49 +0530

    Fourty Two Change

--- src/controllers/userAuthenticate.js
+++ src/controllers/userAuthenticate.js
@@ -14,7 +14,7 @@ const register = async(req,res)=>{
     try{
 
         // Validate for valid emailId and password using validator
-        validate(req.body) ;
+        // validate(req.body) ;
 
         const firstName = req.body.firstName ;
         const emailId = req.body.emailId ;
