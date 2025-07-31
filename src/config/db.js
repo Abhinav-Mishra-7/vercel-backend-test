e287d27c938e1832abd02e49142cb9f03ab51285
Abhinav Mishra <abhinavmishra12514@gmail.com>
2025-07-31 20:07:32 +0530

    Fourty Three Change

--- src/config/db.js
+++ src/config/db.js
@@ -2,8 +2,14 @@ const mongoose = require("mongoose") ;
 
 async function main()
 {
-    
-    await mongoose.connect(process.env.DB_CONNECT_STRING) ;   
+    try {
+        console.log("VERCEL SERVER IS ATTEMPTING TO CONNECT WITH URI:", process.env.DB_CONNECT_STRING);
+        await mongoose.connect(process.env.DB_CONNECT_STRING);
+        console.log("DB Connected");
+    } catch (error) {
+        console.error("DATABASE CONNECTION FAILED:", error);
+        process.exit(1); 
+    }
 } 
 
 module.exports =  main ; 
